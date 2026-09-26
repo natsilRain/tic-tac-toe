83984706c398c9cfbf30cd675563035783e02180
natsilRain <156608986+natsilRain@users.noreply.github.com>
2026-09-26 17:22:47 +0530

    Create win condition logic and game reset logic

--- index.js
+++ index.js
@@ -64,16 +64,24 @@ function createPlayer(playerName) {
 // gameBrain IIFE
 
 const gameBrain = (function () {
-    alert("Welcome to Tic-Tac-Toe!");
+    const gameStart = function () {
+        let boardEmpty = (x) => x === "-";
+        if (!(gameBoard.getBoard().every(boardEmpty))) {
+            gameBoard.createNewBoard();
+        };
 
-    let playerName = prompt("Name of player (X) ?");
-    const player1 = createPlayer(playerName);
+        alert("Welcome to Tic-Tac-Toe!");
 
-    playerName = prompt("Name of player (O) ?");
-    const player2 = createPlayer(playerName);
+        let playerName = prompt("Name of player (X) ?");
+        const player1 = createPlayer(playerName);
 
-    const players = [player1, player2];
+        playerName = prompt("Name of player (O) ?");
+        const player2 = createPlayer(playerName);
 
+        return [player1, player2];
+    };
+
+    let players = gameStart();
     let activePlayer = players[0];
 
     const switchPlayerTurn = () => {
@@ -91,8 +99,73 @@ const gameBrain = (function () {
         console.log(`It's ${getActivePlayer().playerName}'s turn!`);
     };
 
+    const checkWinner = function () {
+        board = gameBoard.getBoard();
+
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] !== "-" && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                return board[i][0];
+            };
+        };
+
+        for (let i = 0; i < 3; i++) {
+            if (board[0][i] !== "-" && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+                return board[0][i];
+            };
+        };
+
+        if (board[1][1] !== "-") {
+            if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+                return board[0][0];
+            };
+            if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+                return board[0][2];
+            };
+        };
+
+        const squaresRemaining = board.some(row => row.includes("-"));
+
+        if (!squaresRemaining) {
+            return "draw";
+        };
+
+        return null;
+    };
+
+    const gameOver = function (player) {
+        alert(`${player.playerName} wins!`);
+        console.log(`${player.playerName} wins!`);
+        
+        const playAgain = confirm("Start new game?");
+
+        if (playAgain) {
+            players = gameStart();
+            activePlayer = players[0];
+        };
+    };
+
     const playTurn = (rowNo, columnNo) => {
         if (gameBoard.insertMark(rowNo, columnNo, getActivePlayer().playerMark)) {
+
+            winner = checkWinner();
+
+            if (winner === "X") {
+                gameOver(players[0]);
+                return;
+            } else if (winner === "O") {
+                gameOver(players[1]);
+                return;
+            } else if (winner === "Draw") {
+                alert("The game is a draw!");
+                console.log("The game is a draw!");
+                const playAgain = confirm("Start new game?");
+
+                if (playAgain) {
+                    players = gameStart();
+                    activePlayer = players[0];
+                };
+            };
+
             switchPlayerTurn();
             displayNewTurn();
         } else {
